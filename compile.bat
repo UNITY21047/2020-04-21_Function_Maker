920fd4d22165314c84755e2af4445e100e92264c
UNITY21047 <unity21047@gmail.com>
2020-04-23 12:25:01 -0500

    Adding all current files.

---
+++ compile.bat
@@ -0,0 +1,7 @@
+cd F:\tests\2020-04-21_Function_Maker
+
+REM Compiles the program
+csc -reference:"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.Windows.Forms.dll" -reference:"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.Drawing.dll" -reference:"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.ComponentModel.dll" -reference:"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.XML.dll" -reference:"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.IO.dll" -out:program.exe program.cs
+
+REM Runs the executable.
+program
---
+++ program.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+namespace Function_Maker
+{
+    class program : Form
+    {
+        
+    }
+}
