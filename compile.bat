61c70897cbbce275ee2fa9b92d49aa15db391ecb
UNITY21047 <unity21047@gmail.com>
2020-04-23 12:37:06 -0500

    Setting up the basics for the form.

--- compile.bat
+++ compile.bat
@@ -1,5 +1,3 @@
-cd F:\tests\2020-04-21_Function_Maker
-
 REM Compiles the program
 csc -reference:"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.Windows.Forms.dll" -reference:"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.Drawing.dll" -reference:"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.ComponentModel.dll" -reference:"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.XML.dll" -reference:"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.IO.dll" -out:program.exe program.cs
 
--- program.cs
+++ program.cs
@@ -3,10 +3,21 @@ using System.Windows.Forms;
 using System.ComponentModel;
 using System.Drawing;
 using System.IO;
+
 namespace Function_Maker
 {
     class program : Form
     {
-        
+        public program() 
+        {
+
+        }
+
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new program());
+        }
     }
 }
